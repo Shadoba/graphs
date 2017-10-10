f669bc935c92fe24a5ad2f14a804cc8bfaeb19f4
Shadoba <firebart1@gmail.com>
2017-10-10 15:13:11 +0200

    Update AdjacencyMatrix.cpp

--- AdjacencyMatrix.cpp
+++ AdjacencyMatrix.cpp
@@ -5,30 +5,6 @@
 #include <cstdlib>
 #include <random>
 #include "time.h"
-// AdjacencyMatrix::AdjacencyMatrix(int peakcount)
-// {
-	// m_PeakCount=peakcount;
-/////////Tworzenie pustej macierzy////////////
-	// m_Matrix=new int *[m_PeakCount];
-	// for(int i=0; i<m_Peakcount; i++)
-	// {
-		// m_Matrix[i]=new int [m_PeakCount];
-	// }
-/////////Uzupe³nainie macierzy////////////////
-	// std::cout<<"podawaj ilosc krawedzi dla wezlow: "<<std::endl;
-	// for (int i=0; i<m_PeakCount; i++)
-	// {
-		// int p=0;
-		// for (int j=p; i<m_PeakCount; j++)
-		// {
-			// std::cout<<i<<"-"<<2<<"		";
-			// std::cin>>m_Matrix[i+1][j+1];
-		// }
-		// p++;
-	// }
-// }
-
-
 
 AdjacencyMatrix::~AdjacencyMatrix()
 {
@@ -37,31 +13,31 @@ AdjacencyMatrix::~AdjacencyMatrix()
         m_Matrix[i].clear();
     }
     m_Matrix.clear();
-    //m_Matrix.~vector();
 }
 
 
-int AdjacencyMatrix::getPeakCount(){
+int AdjacencyMatrix::getPeakCount()
+{
 	return m_PeakCount;
 }
 
-int AdjacencyMatrix::getEdgeCount(){
+int AdjacencyMatrix::getEdgeCount()
+{
 	int n=this->getPeakCount();
 	int edges=0;
-		for(int i=0;i<n;i++){
-			for(int j=0;j<=i;j++){
-
-				edges+=m_Matrix[i][j];
-
-			}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<=i;j++)
+		{
+			edges+=m_Matrix[i][j];
 		}
-		return edges;
-};
+	}
+	return edges;
+}
 
 vector<vector<int> >& AdjacencyMatrix::getMatrix(){
 
 	return m_Matrix;
-};
+}
 
 AdjacencyMatrix::AdjacencyMatrix(std::ifstream & AMatrix)
 {
@@ -70,65 +46,55 @@ AdjacencyMatrix::AdjacencyMatrix(std::ifstream & AMatrix)
 
 	//-------------------------OPERACJE WCZYTYWANIA I ANALIZY PLIKU-----------------------------
 
-	std::getline(AMatrix, CheckPeaks); //Pobranie zawartoœci pliku
+	std::getline(AMatrix, CheckPeaks); //Pobranie zawartoÅ“ci pliku
 
 	//Sprawdzenie czy plik nie jest pusty
 
 	if(CheckPeaks.length()==0){std::perror("Plik jest pusty.");}
 		else {m_PeakCount =1;}
 
-	//Obliczenie liczby wêz³ów
+	//Obliczenie liczby wÃªzÂ³Ã³w
 
 	m_PeakCount+=CheckPeaks.length()/2;
 
 
 	//----------------------------WCZYTANIE MACIERZY Z PLIKU--------------------------
 
-	//Ustawienie fstream na pocz¹tek pliku
+	//Ustawienie fstream na poczÂ¹tek pliku
 	AMatrix.seekg(0);
-	//Poszerzenie macierzy o zadan¹ liczbê wierszy
+	//Poszerzenie macierzy o zadanÂ¹ liczbÃª wierszy
 	m_Matrix.resize(m_PeakCount);
-	//Poszerzenie macierzy o zadan¹ liczbê kolumn oraz wczyranie danych z pliku
-	for (i = 0; i < m_PeakCount; i++) {
+	//Poszerzenie macierzy o zadanÂ¹ liczbÃª kolumn oraz wczyranie danych z pliku
+	for (i = 0; i < m_PeakCount; i++) 
+	{
 		m_Matrix[i].resize(m_PeakCount);
-
-		for (j = 0; j < m_PeakCount; j++) {
+		for (j = 0; j < m_PeakCount; j++) 
+		{
 			AMatrix >> m_Matrix[i][j];
 		}
 	}
-
-//	m_PeakCount = AMatrix[0].size();
-//	m_Matrix.resize(m_PeakCount);
-//	for (int i = 0; i < m_PeakCount; i++)
-//    {
-//        m_Matrix[i].resize(m_PeakCount);
-//		for (int j = 0; j < m_PeakCount; j++)
-//        {
-//			m_Matrix[i][j] = AMatrix[i][j];
-//		}
-//	}
-
-
-};
+}
 
 AdjacencyMatrix::AdjacencyMatrix()
 {
     m_PeakCount=0;
 }
 
-AdjacencyMatrix::AdjacencyMatrix(AdjacencyList & AList){
-
+AdjacencyMatrix::AdjacencyMatrix(AdjacencyList & AList)
+{
 	m_PeakCount=AList.getNodeCount();
 	m_Matrix.resize(m_PeakCount);
 	int i,j;
-	for(i=0;i<m_PeakCount;i++){
+	for(i=0;i<m_PeakCount;i++)
+	{
 		m_Matrix[i].resize(m_PeakCount);
-		for(j=0;j<(int)(AList.getList())[i].size();j++){
+		for(j=0;j<(int)(AList.getList())[i].size();j++)
+		{
 			m_Matrix[i][(AList.getList())[i][j]-1]=1;
 		}
 	}
 
-};
+}
 
 void AdjacencyMatrix::GenarateRandomGraph_N_L(AdjacencyMatrix & AM,int n, double l)
 {
@@ -187,18 +153,11 @@ void AdjacencyMatrix::GenarateRandomGraph_N_L(AdjacencyMatrix & AM,int n, double
                         fprintf(fp,"%lf ", l2);
                         //if(l<0) break;
                     }
-    //            if (l>0 && AM.m_Matrix[i][j]!=1)
-    //                {AM.m_Matrix[i][j]=std::rand()%2; if(AM.m_Matrix[i][j]) l--;}
-    //            else
-    //                AM.m_Matrix[i][j]=0;
-    //                AM.m_Matrix[j][i]=AM.m_Matrix[i][j];
                 }
-          //  if(l>0 && i==AM.m_PeakCount-1) i=0;
             }
         }
         fclose(fp);
     }
-
 }
 
 void AdjacencyMatrix::Clear()
