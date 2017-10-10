b2b65095935eef4f04d1be4866befdd3820ce80c
Shadoba <firebart1@gmail.com>
2017-10-10 15:09:40 +0200

    Update AdjacencyList.cpp

--- AdjacencyList.cpp
+++ AdjacencyList.cpp
@@ -4,54 +4,27 @@
 #include <fstream>
 #include <cstdlib>
 
-
 AdjacencyList::~AdjacencyList(){}
-//	int i=0,j=0; //iteratory
 
-//AdjacencyList::AdjacencyList(std::ifstream & AList)
-//{
-//	std::string CheckPeaks; //Zmienna string przetrzymujaca pierwszy wiersz pliku
-//	m_PeakCount = 0;
-//
-//	//-------------------------OPERACJE WCZYTYWANIA I ANALIZY PLIKU-----------------------------
-//	std::getline(AList, CheckPeaks); //Pobranie zawartoœci pliku
-//	int tmp, tmp2=0, tmp3;
-//	AList.seekg(0);
-//    while (CheckPeaks.length()!=0)
-//	{
-//	    m_PeakCount++;
-//        m_List.resize(PeakCount);
-//        tmp = CheckPeaks.length()/2+1;
-//        tmp2 += CheckPeaks.length();
-//
-//	    for(int i=0; i<tmp; i++)
-//        {
-//            AList>>tmp3;
-//            m_List[m_PeakCount-1].push_back(tmp3);
-//        }
-//        std::getline(AList, CheckPeaks); //Pobranie zawartoœci pliku
-//        AList.seekg(tmp2);
-//	}
-//}
 AdjacencyList::AdjacencyList(std::ifstream& AList)
 {
 	std::string tmp;
 	unsigned int it;
 	m_NodeCount=0;
 	while(!AList.eof())
-    {
+    	{
 		std::getline(AList, tmp);
 		m_NodeCount++;
 		m_List.resize(m_NodeCount);
 		for( it=0 ; it < tmp.length() ; it+=2 )
-        {
+        	{
 			m_List[m_NodeCount-1].push_back( atoi( &tmp[it] ) );
 		}
 	}
 }
 
-AdjacencyList::AdjacencyList(IncidanceMatrix & IMatrix){
-
+AdjacencyList::AdjacencyList(IncidanceMatrix & IMatrix)
+{
 	m_List.resize(IMatrix.getPeakCount());
 	m_NodeCount=IMatrix.getPeakCount();
 	int k;
@@ -60,12 +33,13 @@ AdjacencyList::AdjacencyList(IncidanceMatrix & IMatrix){
 	fir=-1;
 	sec=0;
 	for(i=0;i<k;i++)
-    {
-		for(j=0;j<m_NodeCount;j++){
-			if((IMatrix.getMatrix())[j][i]==1){
+ 	{
+		for(j=0;j<m_NodeCount;j++)
+		{
+			if((IMatrix.getMatrix())[j][i]==1)
+			{
 				if(fir==-1){fir=j;}
 				sec=j;
-
 			}
 		}
 		m_List[fir].push_back(sec+1);
@@ -75,15 +49,14 @@ AdjacencyList::AdjacencyList(IncidanceMatrix & IMatrix){
 		sec=-1;
 	}
 
-};
+}
 
-int AdjacencyList::getNodeCount(){
+int AdjacencyList::getNodeCount()
+{
 	return m_NodeCount;
-};
+}
 
-
-vector<vector<int> >& AdjacencyList::getList(){
-
+vector<vector<int> >& AdjacencyList::getList()
+{
 	return m_List;
-
-};
+}
